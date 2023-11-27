89f371daa63c46a42e400e00bd8dadc5326a3b7f
rafaelfmarques <rafaelmarquesclps9@gmail.com>
2023-11-27 02:45:48 -0300

    feat: add public hello world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from './auth/utils/constants';
 
 @Controller()
 @ApiTags('APP')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
